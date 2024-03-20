2f5b48b6825f8c7981c5f23d409ccac2587c55e0
Eva <530028084@qq.com>
2024-03-20 11:35:35 +0800

    java proxy: implement invocationHandler and emphasize the interface must exist.

--- src/main/java/org/proxy/JavaProxyTest.java
+++ src/main/java/org/proxy/JavaProxyTest.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Proxy;
 public class JavaProxyTest {
 
     public static void main(String[] args) {
+        //JDK 会在运行时动态帮你生成一个新的类。
         Object proxyInstance = Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[]{EchoService.class}, new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
@@ -22,6 +23,7 @@ public class JavaProxyTest {
             }
         });
 
+        //生成的这个代理类继承了java的proxy类并且实现了被代理的接口，而那个接口甚至可以不定义实现类，但是接口必须存在。
         EchoService echoServiceProxy = (EchoService) proxyInstance;
         ((EchoService) proxyInstance).echoInformation("Eva interview smoothly!");
     }
